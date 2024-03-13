9d70213b898b174623da1107012c20651aad2622
Ihor <igsu2015@gmail.com>
2024-03-13 14:06:07 +0100

    Add project files.

---
+++ SomerenModel/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace SomerenModel
+{
+    public class Teacher
+    {
+        public int Id { get; set; }     // database id
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Number { get; set; } // LecturerNumber, e.g. 47198
+    }
+}

35890bf806de605f301e8b2662aed296bdfc14d6
Jacobo Martínez Llansó <713379@student.inholland.nl>
2024-03-21 15:26:43 +0100

    ..

--- SomerenModel/Room.cs
+++ SomerenModel/Room.cs
@@ -3,8 +3,10 @@
     public class Room
     {
         public int Id { get; set; }         // database id
+
+        public string Building { get; set; } // database building
         public int Number { get; set; }     // RoomNumber, e.g. 206
         public int Capacity { get; set; }   // number of beds, either 4, 6, 8, 12 or 16
-        public bool Type { get; set; }      // student = false, teacher = true
+        public string RoomType { get; set; }      //singular or dormitory
     }
 }
